6a3d10aeb34a5ea9d3fb5860afb740e6180e73ce
Satyajit Bulage <sbulage@redhat.com>
2017-04-29 15:15:00 +0530

    Added subtraction functionality

--- calc.py
+++ calc.py
@@ -7,7 +7,10 @@ class Calculator:
     def add(self, x, y):
         return x + y
 
+    def sub(self, x, y):
+        return x - y
 
 if __name__ == '__main__':
     cal = Calculator()
     print(cal.add(2, 3))
+    print(cal.sub(5, 3))
