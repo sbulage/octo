9409aba3eaca25a5f418592b7a3da47aaffecadf
Nikhil Kathole <nikhilkathole2683@gmail.com>
2017-04-29 09:33:09 +0000

    added multiplication

--- calc.py
+++ calc.py
@@ -7,7 +7,11 @@ class Calculator:
     def add(self, x, y):
         return x + y
 
+    def mul(self, x, y):
+        return x * y
+
 
 if __name__ == '__main__':
     cal = Calculator()
-    print(cal.add(2, 3))
+print(cal.add(2, 3))
+print(cal.mul(2, 3))
