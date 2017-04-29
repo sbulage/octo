e6b2a2a252cde6eb942e785eeadeef99f2b0be89
Aditya <aditya@example.com>
2017-04-29 15:22:24 +0530

    added multiplication function

--- calc.py
+++ calc.py
@@ -7,10 +7,10 @@ class Calculator:
     def add(self, x, y):
         return x + y
 
-    def sub(self, x,y):
-	return x-y
+    def mul(self, x,y):
+	return x*y
 
 if __name__ == '__main__':
 	cal = Calculator()
 	print(cal.add(2, 3))
-	print(cal.sub(5, 3))	
+	print(cal.mul(5, 3))	
