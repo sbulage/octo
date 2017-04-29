2417bb6638e60c6881812fe2aefba4a9ae7700cb
aishwarya.8421@gmail.com <aishu@localhost.localdomain>
2017-04-29 15:33:54 +0530

    addition subtraction finally commited

--- calc.py
+++ calc.py
@@ -6,8 +6,10 @@ class Calculator:
 
     def add(self, x, y):
         return x + y
-
+    def sub(self, x, y):
+	return x - y
 
 if __name__ == '__main__':
     cal = Calculator()
     print(cal.add(2, 3))
+    print(cal.add(2,3))
