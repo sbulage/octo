4d06500cea104cdeec2f7a6464f3a5f73b4d1f5c
Durgesh <durgeshreddiyar@yahoo.com>
2017-04-29 15:10:22 +0530

    added fuctionality

--- calc.py
+++ calc.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 class Calculator:
-    def __init__(self):
-        pass
+	def __init__(self):
+        	pass
 
-    def add(self, x, y):
-        return x + y
-
+	def add(self, x, y):
+        	return x + y
+	def sub(self,x,y):
+		return x-y
+	
 
 if __name__ == '__main__':
     cal = Calculator()
     print(cal.add(2, 3))
+	print(cal.sub(2,3))
