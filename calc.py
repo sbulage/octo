4cc7ebbc91e24f84e76ec79c19da0a1d32d945b3
Yogiraj <karajagiyogiraj@gmail.com>
2017-04-29 15:04:27 +0530

    This is modified octo

--- calc.py
+++ calc.py
@@ -4,10 +4,10 @@ class Calculator:
     def __init__(self):
         pass
 
-    def add(self, x, y):
-        return x + y
+    def sub(self, x, y):
+        return x - y
 
 
 if __name__ == '__main__':
     cal = Calculator()
-    print(cal.add(2, 3))
+    print(cal.sub(5, 3))
