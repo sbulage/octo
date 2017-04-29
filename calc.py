19c86beceb5726c9de60f45539847459f6330443
shitalkanoje.sk@gmail.com <rit@localhost.localdomain>
2017-04-29 15:33:35 +0530

    add substraction

--- calc.py
+++ calc.py
@@ -6,8 +6,10 @@ class Calculator:
 
     def add(self, x, y):
         return x + y
-
+    def diff(self,x,y):
+        return x-y
 
 if __name__ == '__main__':
     cal = Calculator()
     print(cal.add(2, 3))
+    print(cal.diff(2,3))
