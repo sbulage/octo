445279283c8be1441faba1a8c020ae16e079ad5e
root <root@rit-OptiPlex-3020.(none)>
2017-04-29 15:15:12 +0530

    Added sub and mul

--- calc.py
+++ calc.py
@@ -7,7 +7,15 @@ class Calculator:
     def add(self, x, y):
         return x + y
 
+    def sub(self,x,y):
+	return x-y
+
+    def mul(self,x,y):
+	return x*y
+
 
 if __name__ == '__main__':
     cal = Calculator()
     print(cal.add(2, 3))
+    print(cal.sub(2, 3))
+    print(cal.mul(2, 3))
